99ffed20427fd5a4b0c88a9ca8d58f55da9b76f7
R1ple7 <draxmonjonov0@gmail.com>
2026-07-09 17:39:48 +0300

    Add layout components: Header, Sidebar, Footer and dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { ReactNode } from 'react'
 import { Sidebar } from '@/components/layout/Sidebar'
 import { Header } from '@/components/layout/Header'
 import { Footer } from '@/components/layout/Footer'
